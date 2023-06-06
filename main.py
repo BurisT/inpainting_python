ca2524247edccab3b52f2999ab37a8d1e45ef097
BurisT <book_buris@hotmail.com>
2023-06-06 20:15:11 +0200

    add1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from inpainting.discrete_gradient_operator import L,L_trans
 import matplotlib.pyplot as plt
 
 def main():
-    print('test')
+    print('test_1')
     plt.plot(np.arange(10), np.arange(10))
     plt.show()
 if __name__ == '__main__':
