ac5cead9e5e25aadb97df562478813058ffb2e46
BurisT <book_buris@hotmail.com>
2023-06-06 20:07:58 +0200

    test commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import numpy as np
-print(np.__version__)
-print('Hello numpy')
+from inpainting.discrete_gradient_operator import L,L_trans
+import matplotlib.pyplot as plt
+
+def main():
+    print('test')
+    plt.plot(np.arange(10), np.arange(10))
+    plt.show()
+if __name__ == '__main__':
+    main()
