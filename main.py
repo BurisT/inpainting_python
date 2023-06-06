7fe3653b16e6568e53349790bc9910368b6071af
BurisT <book_buris@hotmail.com>
2023-06-06 20:19:13 +0200

    Revert "add1"
    This reverts commit ca2524247edccab3b52f2999ab37a8d1e45ef097.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from inpainting.discrete_gradient_operator import L,L_trans
 import matplotlib.pyplot as plt
 
 def main():
-    print('test_1')
+    print('test')
     plt.plot(np.arange(10), np.arange(10))
     plt.show()
 if __name__ == '__main__':
